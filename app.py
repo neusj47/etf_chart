1ae1beece208421c40c99ebee57c7721d49e76a4
neusj47 <neusj47@gmail.com>
2021-07-06 23:57:48 +0900

    # commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # ETF 별 누적수익률 추이를 호출하는 함수
 
 from pykrx import stock
+import FinanceDataReader as fdr
+import pandas as pd
 import requests
 import json
 from pandas.io.json import json_normalize
@@ -12,6 +14,46 @@ import dash_html_components as html
 import plotly.express as px
 import dash_table
 from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+
+# ESG ETF 리스트
+df_etf_kr = fdr.EtfListing('KR')
+df_esg_etf_list = df_etf_kr[df_etf_kr['Name'].str.contains('ESG')] # ETF > ESG 리스트
+df_kospi_kr = fdr.StockListing('KOSPI') # KOSPI리스트
+df_kosdaq_kr = fdr.StockListing('KOSDAQ') # KOSPI리스트
+df_ks_kr = pd.concat([df_kospi_kr, df_kosdaq_kr], ignore_index = True)
+df_stock_list = df_ks_kr.dropna(subset=['Sector']) # KR > 종목리스트
+
+
+std_date = '20210101'
+TICKER = '289040' # KODEX MSCI KOREA ESG유니버설
+
+def get_ETF_PDF(TICKER, std_date):
+    df = stock.get_etf_portfolio_deposit_file(TICKER, std_date)
+    df = df.reset_index(drop=False)
+    df = df.rename(columns={'티커':'Symbol'})
+    df = pd.merge(df,df_stock_list)
+    df['기준가'] = round(df['금액'] /df['계약수'])
+    df = df[['Symbol','Name','Market','Sector','기준가','계약수','금액','비중']]
+    df = df.rename(columns={'Symbol':'TICKER','Name':'종목명'})
+    df = df.reset_index(drop=True)
+    ETF_PDF = df
+    return ETF_PDF
+
+ETF_PDF = get_ETF_PDF('289040','20210628')
+
+def get_ETF_return(start_date, end_date, TICKER):
+    df = stock.get_etf_ohlcv_by_date(start_date, end_date, TICKER)
+    df = df.reset_index(drop=False)
+    df['Symbol'] = TICKER
+    df = pd.merge(df,df_esg_etf_list, how = "inner", on = "Symbol")
+    df = df[['날짜','Symbol','Name','종가','NAV','거래량','기초지수']]
+    df = df.rename(columns={'Symbol':'TICKER','Name':'ETF명'})
+    df['수익률'] = (df['종가'] / df['종가'].shift(1)) - 1
+    df['누적수익률'] = (1 + df['수익률']).cumprod() - 1
+    ETF_return = df.dropna(axis=0)
+    return ETF_return
 
 
 def get_ESG_data(start_date,end_date):
@@ -58,60 +100,231 @@ df_fig = px.line(df_fig, x='날짜', y='누적수익률', color = '종목코드'
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("ESG Dash", className="display-4"),
+        html.Hr(),
+        html.P(
+            "ETF & Index", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("ETF PDF", href="/", active="exact"),
+                dbc.NavLink("ETF 누적수익률", href="/page-1", active="exact")
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
 
 app.layout = html.Div([
-    html.H3("ESG ETF 누적 수익률 추이 "),
-    dcc.DatePickerRange(
-        id="my-date-picker-range",
-        min_date_allowed=date(2015, 1, 1),
-        start_date_placeholder_text='20200101',
-        end_date_placeholder_text='20210628',
-        display_format='YYYYMMDD'
-    ),
-    dcc.Graph(
-        id = 'my-graph',
-        style={'height': 600},
-        figure = df_fig
-    ),
-    dash_table.DataTable(
-        id="datatable-interactivity",
-        columns=[
-            {
-                "name": i,
-                "id": i,
-                "deletable": False,
-                "selectable": True,
-                "hideable": False,
-            }
-            for i in df_price.columns
-        ],
-        data=df_price.to_dict("records"),  # the contents of the table
-        sort_action="native",  # enables data to be sorted per-column by user or not ('none')
-        sort_mode="single",  # sort across 'multi' or 'single' columns
-        page_action="native",  # all data is passed to the table up-front or not ('none')
-        page_size=15,  # number of rows visible per page
-        style_cell={  # ensure adequate header width when text is shorter than cell's text
-            "minWidth": 150,
-            "maxWidth": 250,
-            "width": 200,
-        },
-        style_data={  # overflow cells' content into multiple lines
-            "whiteSpace": "normal",
-            "height": "auto",
-        },
-        style_data_conditional=[
-            {
-                'if': {'row_index': 'odd'},
-                'backgroundColor': 'rgb(248, 248, 248)'
-            }
-        ],
-        style_header={
-            'backgroundColor': 'rgb(230, 230, 230)',
-            'fontWeight': 'bold'
-        }
-    )
+    dcc.Location(id="url"),
+    sidebar,
+    content
 ])
+
+
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#
+# app.layout = html.Div([
+#     html.H3("ESG ETF 누적 수익률 추이 "),
+#     dcc.DatePickerRange(
+#         id="my-date-picker-range",
+#         min_date_allowed=date(2015, 1, 1),
+#         start_date_placeholder_text='20200101',
+#         end_date_placeholder_text='20210628',
+#         display_format='YYYYMMDD'
+#     ),
+#     dcc.Graph(
+#         id = 'my-graph',
+#         style={'height': 600},
+#         figure = df_fig
+#     ),
+#     dash_table.DataTable(
+#         id="datatable-interactivity",
+#         columns=[
+#             {
+#                 "name": i,
+#                 "id": i,
+#                 "deletable": False,
+#                 "selectable": True,
+#                 "hideable": False,
+#             }
+#             for i in df_price.columns
+#         ],
+#         data=df_price.to_dict("records"),  # the contents of the table
+#         sort_action="native",  # enables data to be sorted per-column by user or not ('none')
+#         sort_mode="single",  # sort across 'multi' or 'single' columns
+#         page_action="native",  # all data is passed to the table up-front or not ('none')
+#         page_size=15,  # number of rows visible per page
+#         style_cell={  # ensure adequate header width when text is shorter than cell's text
+#             "minWidth": 150,
+#             "maxWidth": 250,
+#             "width": 200,
+#         },
+#         style_data={  # overflow cells' content into multiple lines
+#             "whiteSpace": "normal",
+#             "height": "auto",
+#         },
+#         style_data_conditional=[
+#             {
+#                 'if': {'row_index': 'odd'},
+#                 'backgroundColor': 'rgb(248, 248, 248)'
+#             }
+#         ],
+#         style_header={
+#             'backgroundColor': 'rgb(230, 230, 230)',
+#             'fontWeight': 'bold'
+#         }
+#     )
+# ])
+
+
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")]
+)
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.Div([
+    dcc.Tabs([
+        dcc.Tab(label = "ETF PDF", children = [
+            html.H3("ESG별 ETF 정보"),
+            dcc.DatePickerSingle(
+                    id="my-date-picker-single",
+                    min_date_allowed=date(2015, 1, 1),
+                    initial_visible_month=date(2021, 6, 28),
+                    display_format='YYYYMMDD'
+                    ),
+            dcc.Graph(
+                id = 'my-graph',
+                style={'height': 600},
+                figure = df_fig
+            ),
+            dash_table.DataTable(
+                id="datatable-interactivity",
+                columns=[
+                    {
+                        "name": i,
+                        "id": i,
+                        "deletable": True,
+                        "selectable": True,
+                        "hideable": False,
+                    }
+                    for i in ETF_PDF.columns
+                ],
+                data=ETF_PDF.to_dict("records"),  # the contents of the table
+                sort_action="native",  # enables data to be sorted per-column by user or not ('none')
+                sort_mode="single",  # sort across 'multi' or 'single' columns
+                page_action="native",  # all data is passed to the table up-front or not ('none')
+                page_size=30,  # number of rows visible per page
+                style_cell={  # ensure adequate header width when text is shorter than cell's text
+                    "minWidth": 95,
+                    "maxWidth": 200,
+                    "width": 95,
+                },
+                # style_cell_conditional={
+                #     'textAlign': 'left'
+                # },
+                style_data={  # overflow cells' content into multiple lines
+                    "whiteSpace": "normal",
+                    "height": "auto",
+                },
+                style_data_conditional=[
+                    {
+                        'if': {'row_index': 'odd'},
+                        'backgroundColor': 'rgb(248, 248, 248)'
+                    }
+                ],
+                style_header={
+                    'backgroundColor': 'rgb(230, 230, 230)',
+                    'fontWeight': 'bold'
+                })
+            ]),
+            dcc.Tab(label="KOSPI", children=[
+                html.H3("종목 주가 정보"),
+                dash_table.DataTable(
+                    id="datatable-interactivity_secu",
+                    columns=[
+                        {
+                            "name": i,
+                            "id": i,
+                            "deletable": False,
+                            "selectable": True,
+                            "hideable": False,
+                        }
+                        for i in df_price.columns
+                    ],
+                    data=df_price.to_dict("records"),  # the contents of the table
+                    sort_action="native",  # enables data to be sorted per-column by user or not ('none')
+                    sort_mode="single",  # sort across 'multi' or 'single' columns
+                    page_action="native",  # all data is passed to the table up-front or not ('none')
+                    page_size=20,  # number of rows visible per page
+                    style_cell={  # ensure adequate header width when text is shorter than cell's text
+                        "minWidth": 95,
+                        "maxWidth": 200,
+                        "width": 95,
+                    },
+                    # style_cell_conditional={
+                    #     'textAlign': 'left'
+                    # },
+                    style_data={  # overflow cells' content into multiple lines
+                        "whiteSpace": "normal",
+                        "height": "auto",
+                    },
+                    style_data_conditional=[
+                        {
+                            'if': {'row_index': 'odd'},
+                            'backgroundColor': 'rgb(248, 248, 248)'
+                        }
+                    ],
+                    style_header={
+                        'backgroundColor': 'rgb(230, 230, 230)',
+                        'fontWeight': 'bold'
+                    })
+                ])
+        ])
+        ])
+    elif pathname == "/page-1":
+        return [
+            html.H1('Grad School in Iran',
+                    style={'textAlign': 'center'}),
+            dcc.Graph(id='bargraph',
+                      figure=px.bar(df, barmode='group', x='Years',
+                                    y=['Girls Grade School', 'Boys Grade School']))
+        ]
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
 
 
 @app.callback(
